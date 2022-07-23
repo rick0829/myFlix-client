9c436816847a2bb8caaea59607522cc66ccc5ed4
rick0829 <earl0829@gmail.com>
2022-07-23 13:55:54 -0700

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import axios from 'axios';
-import MovieCard from '../movie-card/movie-card';
+import { LoginView } from '../login-view/login-view';
+import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { RegistrationView } from '../registration-view/registration-view';
 
 export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
       movies: [],
-      selectedMovie: null
+      selectedMovie: null,
+      user: null,
+      registered: null
     };
   }
 
@@ -33,7 +37,7 @@ export class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
 
-
+    if (selectedMovie) return <MovieView movie={selectedMovie} />;
     if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
 
     return (
